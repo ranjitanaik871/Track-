0fd26254185c8752e4b0a02d970bc3c067447eec
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-07 14:52:47 +0530

    new commit

--- TaskB.py
+++ TaskB.py
@@ -1,5 +1,6 @@
-#sales taem ,sales figure
 sales_figure=[1000,500,20,60,800,700]
+
+print("sales figure:",sales_figure)
 
 print(' '*20)
 #list sorting in ascending using sort()
@@ -9,3 +10,4 @@ print(' '*20)
 #list sorting in descending order using reverse()
 sales_figure.reverse()
 print("sorted in descending:",sales_figure)
+
---
+++ TaskF.py
@@ -0,0 +1,11 @@
+#Registered list for webinar
+list=["U15","U21","U16","u32","U15","U56"]
+print("Registered list",list)
+
+#removing the duplicate entry
+a=list.remove("U15")
+print("Removed list",list)
+
+#using clear once the event is completed
+list.clear()
+print(list)
