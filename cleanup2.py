a79496bb1ccdc50e6433b03c854f157e440e6245
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-11 23:25:08 +0530

    modified cleanup file

---
+++ cleanup2.py
@@ -0,0 +1,13 @@
+sales_figure=[1000,500,20,60,800,700]
+
+print("sales figure:",sales_figure)
+
+print(' '*20)
+#list sorting in ascending using sort()
+sales_figure.sort()
+print("sorted in ascending:",sales_figure)
+print(' '*20)
+#list sorting in descending order using reverse()
+sales_figure.reverse()
+print("sorted in descending:",sales_figure)
+
