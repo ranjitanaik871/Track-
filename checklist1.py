1aa841b52c4e571b13ba558fb34620caf9b3233d
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-14 23:10:02 +0530

    updated

--- checklist1.py
+++ checklist1.py
@@ -1,8 +1,12 @@
+#employee boarding checklist
+#task issued in the list
 task=["laptop issued","id card issued","email issued"]
 print("list of task:",task)
 
+#use append to add the new task
 task.append("compliance training")
 print("new task:",task)
 
+#using insert(), can add the urgent task using index
 new=task.insert(0,"writing")
 print("urgent task:",task)
