cae426f162570cb82e0a9f55c04dba67d6b36565
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-14 23:17:34 +0530

    updated

--- queue4.py
+++ queue4.py
@@ -1,14 +1,16 @@
+#Task assignment queue
+#pending task
 tasks=["paper writing","reading","mailing","meeting","managing"]
 print("Task:",tasks)
 print(' '*20)
-
+#uses pop to assign next task(from the front of queue)
 assigned=tasks.pop(3)
 print("assigned:",assigned)
 print("remaining tasks in queue:",tasks)
 print(' '*20)
-
+#added new task
 new_task=["database","backuping","email"]
 print("New task:",new_task)
-
+#display with new and all added task
 tasks.extend(new_task)
 print(tasks)
