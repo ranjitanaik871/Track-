ce734c15ba6c14ff44c75350b579ebafb2c8f5fc
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-14 23:42:08 +0530

    added

---
+++ emptask1.py
@@ -0,0 +1,9 @@
+#employee directory builder
+#using list and loop
+name=["roh","soh","hoh","noh","kol","wes","des"]
+#serial number
+count=1
+#takes emp names
+for i in name:
+    print(count,i)
+    count+=1
