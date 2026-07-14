05d7114fee0fd6e9f352f423c5813d8137e99087
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-14 15:09:48 +0530

    added

---
+++ add.py
@@ -0,0 +1,10 @@
+def addition(a,b):
+    sum=a+b
+    print("result:",sum)
+addition(5,6)
+
+#using positional arguments
+def student(name,age):
+    print("name:",name)
+    print("age:",age)
+student("ranjita",20)
