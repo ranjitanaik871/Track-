d9de5451b8a117e2d9057e1b51a374dea1a4342b
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-14 23:30:05 +0530

    updated

--- GPS7.py
+++ GPS7.py
@@ -1,3 +1,7 @@
+#GPS location logger
+#fixed longitude,latitude of delivery stops
 delivery=[(77.9176,88.216),(89.7169,12.1412),(916.823,816.234)]
+
+#accessing individual latitude/longitude using index
 for i in range(len(delivery)):
     print("stop",i+1,":",delivery[i])
