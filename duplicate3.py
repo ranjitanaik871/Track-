4b9258bf648f6303c20bf7a364e0779240bba8cc
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-14 23:16:29 +0530

    updated

--- duplicate3.py
+++ duplicate3.py
@@ -1,8 +1,14 @@
-product_code=["c21","b32","j32","b21","c21"]
-code=input("enter the product code:")
-print("count:",product_code.count(code))
-if code in product_code:
+#inventory duplicate finder
+#duplicate product code
+product_code=["c21","b32","j32","b21","c21","b32","t21","t21"]
 
+#take from user, from product code
+code=input("enter the product code:")
+#using count ,how many times repeted 
+print("count:",product_code.count(code))
+
+#checks the condition
+if code in product_code:
    print("the first occurance of product code",product_code.index(code))
 else:
    print("product code not found")
