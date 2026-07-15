07b3c91d214db0cbc9b610968d2c9540d907ef6e
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-15 15:12:57 +0530

    added

--- emp_bonus7.py
+++ emp_bonus7.py
@@ -1,18 +1,17 @@
 # Employee Bonus Eligibility Function
-
-def check_bonus(years_of_experience, performance_rating):
-    if years_of_experience > 2 and performance_rating >= 8:
+def check_bonus(experience,rating):
+    #used conditional statement
+    if experience > 2 and rating >= 8:
         return "Eligible"
     else:
         return "Not Eligible"
 
 #for printing different employees using range
-for i in range(5):
+for i in range(3):
     print("Employee", i + 1)
 
     experience = int(input("Enter years of experience: "))
     rating = int(input("Enter performance rating: "))
 
     result = check_bonus(experience, rating)
-
     print("Bonus:", result)
